a4955b14c92e18ed4d87aec2c8cb6647ee99d34a
badb100d <260448993@qq.com>
2023-05-08 11:50:59 +0800

    Update hwrng auto config

--- bashrc_add.sh
+++ bashrc_add.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # check if character special file hwrng exists.
 if [ -c /dev/hwrng ];then
-    RAND_GEN="/dev/hwrng"
+    GENPASSWD="[ 0 -eq \$(id -u) ] && alias genpasswd=\"strings /dev/hwrng | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n';echo\" || alias genpasswd=\"strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n';echo\""
 else
-    RAND_GEN="/dev/urandom"
+    GENPASSWD="alias genpasswd=\"strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n';echo\""
 fi
-
+#DOCKER_RC="if [ \$\$ == 1 ]; then
+#    nohup /etc/rc.local  >>/var/log/rc.log 2>&1 &
+#fi"
 cat <<EOF >> /etc/bash.bashrc
 export HISTTIMEFORMAT="%y/%m/%d %T "
-alias genpasswd="strings $RAND_GEN | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n';echo"
+$GENPASSWD
 extract() {
          if [ -f \$1 ] ; then
                  case \$1 in
@@ -34,7 +36,7 @@ alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^
 diskusg() { du -b --max-depth 1 | sort -nr | perl -pe 's{([0-9]+)}{sprintf "%.1f%s", \$1>=2**30? (\$1/2**30, "G"): \$1>=2**20? (\$1/2**20, "M"): \$1>=2**10? (\$1/2**10, "K"): (\$1, "") }e'|column -t;}
 alias meminfo='free -m -l -t'
 alias ps?="ps -ef|grep"
-alias busy="cat $RAND_GEN| hexdump -C | grep 'ca fe'"
+alias busy="cat /dev/urandom | hexdump -C | grep 'ca fe'"
 alias wttr="curl wttr.in"
 alias ll='ls -lF --color=auto'
 alias la='ls -la --color=auto'
